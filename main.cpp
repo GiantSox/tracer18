0988a9a14bd68221f7dffa3b3c56276ea1472fd8
David Goodman <GiantSox@users.noreply.github.com>
2021-08-01 12:01:28 -0700

    3 years later so no idea what this is :D

--- main.cpp
+++ main.cpp
@@ -258,11 +258,13 @@ int main() {
 	// position, radius, surface color, reflectivity, transparency, emission color
 	//spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
 	spheres.push_back(Sphere(Vec3f(0.0, 0, -25), 4, Vec3f(1.00, 0.32, 0.36), 0, 0));
-	spheres.push_back(Sphere(Vec3f(5.0, -2, -17.5), 2, Vec3f(0.90, 0.76, 0.46), 0, 0.0));
-	spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 0, 0.0));
+	spheres.push_back(Sphere(Vec3f(6.0, -2, -17.5), 2, Vec3f(0.90, 0.76, 0.46), 0, 0.0));
+	spheres.push_back(Sphere(Vec3f(5.5, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 0, 0.0));
 	spheres.push_back(Sphere(Vec3f(-5.75, 0, -17), 3, Vec3f(0.90, 0.90, 0.90), 0, 0.0));
 	// light
+//	spheres.push_back(Sphere(Vec3f(0.0, -20, -37.5), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
 	spheres.push_back(Sphere(Vec3f(0.0, 20, -30), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
+	spheres.push_back(Sphere(Vec3f(0.0, -20, -30), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
 
 	
 
